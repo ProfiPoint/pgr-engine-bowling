6d2b7f26a37b1f849d70cebc90e2a1436d5ab37e
Ondra Copak <ondra.copak@gmail.com>
2026-04-08 01:42:50 +0200

    obj loader

---
+++ geometry/objLoader.cpp
@@ -0,0 +1,102 @@
+#include "objLoader.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// implemented .obj parser from: MIT https://github.com/dartzon/dotObj-parser (the remapBuffers)
+namespace copakond {
+    std::vector<std::string> splitString(const std::string& str) { // https://stackoverflow.com/questions/5167625
+        std::vector<std::string> tokens;
+        std::stringstream ss(str);
+        std::string token;
+        while (ss >> token) {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+
+    ObjLoader::ObjLoader(const std::string& fileName) {
+        if (fileName.length() < 4 || fileName.substr(fileName.length() - 4) != ".obj") {
+            std::cerr << "File must be .obj: " << fileName << std::endl;
+            exit(1);
+        }
+
+        std::ifstream file(fileName);
+        if (!file.is_open()) {
+            std::cerr << "Error opening file " << fileName << std::endl;
+            exit(1);
+        }
+
+        std::vector<float> vertices;
+        std::vector<float> normals;
+        std::vector<std::string> faces;
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::vector<std::string> data = splitString(line);
+            if (data.empty()) continue;
+
+            if (data[0] == "v" && data.size() >= 4) {
+                vertices.push_back(std::stof(data[1]));
+                vertices.push_back(std::stof(data[2]));
+                vertices.push_back(std::stof(data[3]));
+            }
+            else if (data[0] == "vn" && data.size() >= 4) {
+                normals.push_back(std::stof(data[1]));
+                normals.push_back(std::stof(data[2]));
+                normals.push_back(std::stof(data[3]));
+            }
+            else if (data[0] == "f" && data.size() >= 4) {
+                faces.push_back(data[1]);
+                faces.push_back(data[2]);
+                faces.push_back(data[3]);
+            }
+        }
+
+        remapBuffers(vertices, normals, faces);
+    }
+
+    // reorders the vertices and normals, so each index has the same data. Implementation of dartzon's obj parser.
+    void ObjLoader::remapBuffers(const std::vector<float>& tempV, const std::vector<float>& tempVn, const std::vector<std::string>& rawFaces) {
+        std::map<std::string, unsigned int> history;
+
+        for (const std::string& vertexCombo : rawFaces) {
+            if (history.count(vertexCombo) > 0) { // already parsed to history
+                faces.push_back(history[vertexCombo]);
+
+            } else { // new - processes
+                int vIndex = 0;
+                int nIndex = 0;
+                if (sscanf(vertexCombo.c_str(), "%d//%d", &vIndex, &nIndex) == 2) { // parses the face string in format vertexX//normalX  (x/y/z)
+                    int vBase = (vIndex - 1) * 3;
+                    int nBase = (nIndex - 1) * 3;
+
+                    vertices.push_back(tempV[vBase]);
+                    vertices.push_back(tempV[vBase + 1]);
+                    vertices.push_back(tempV[vBase + 2]);
+
+                    normals.push_back(tempVn[nBase]);
+                    normals.push_back(tempVn[nBase + 1]);
+                    normals.push_back(tempVn[nBase + 2]);
+
+                    unsigned int newIndex = (vertices.size() / 3) - 1;
+                    history[vertexCombo] = newIndex;
+                    faces.push_back(newIndex);
+                }
+            }
+        }
+    }
+
+    const std::vector<float>& ObjLoader::getVertices() const {
+        return vertices;
+    }
+
+    const std::vector<float>& ObjLoader::getNormals() const {
+        return normals;
+    }
+
+    const std::vector<unsigned int>& ObjLoader::getFaces() const {
+        return faces;
+    }
+}
---
+++ geometry/objLoader.h
@@ -0,0 +1,26 @@
+#ifndef PGR_SEM_COPAKOND_OBJLOADER_H
+#define PGR_SEM_COPAKOND_OBJLOADER_H
+#include <string>
+#include <vector>
+
+#include <map> // Added for our history tracker
+
+namespace copakond {
+    class ObjLoader {
+    private:
+        std::vector<float> vertices;
+        std::vector<float> normals;
+        std::vector<unsigned int> faces;
+
+        void remapBuffers(const std::vector<float>& vertices, const std::vector<float>& normals, const std::vector<std::string>& faces);
+
+    public:
+        ObjLoader(const std::string& fileName);
+
+        const std::vector<float>& getVertices() const;
+        const std::vector<float>& getNormals() const;
+        const std::vector<unsigned int>& getFaces() const;
+    };
+}
+
+#endif //PGR_SEM_COPAKOND_OBJLOADER_H
