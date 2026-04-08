39737432d8b6b7f8de9c885bb79d1328b54daeb8
Ondra Copak <ondra.copak@gmail.com>
2026-04-08 02:13:25 +0200

    fixed input map keys UPOPERCASE and lowercase are now the same.

--- utils/input.cpp
+++ utils/input.cpp
@@ -10,13 +10,15 @@ namespace copakond {
 
     // saving input to key map once some key is pressed and unpressed
     void Input::keyboardInputEvent(unsigned char key, int x, int y) {
-        keys_map[key] = true;
+        keys_map[std::tolower(key)] = true;
+        keys_map[std::toupper(key)] = true;
     }
     void Input::specKeyboardInputEvent(int key, int x, int y) {
         keys_map[key+256] = true;
     }
     void Input::keyboardUpInputEvent(unsigned char key, int x, int y) {
-        keys_map[key] = false;
+        keys_map[std::tolower(key)] = false;
+        keys_map[std::toupper(key)] = false;
     }
     void Input::specKeyboardUpInputEvent(int key, int x, int y) {
         keys_map[key+256] = false;
@@ -24,10 +26,10 @@ namespace copakond {
 
     // parsing key movement WASD / arrows
     void Input::keyInput(float deltaTime) {
-        if (keys_map['w'] || keys_map['W']) camera.processKeyboard(FRONT, deltaTime);
-        if (keys_map['s'] || keys_map['S']) camera.processKeyboard(BACK, deltaTime);
-        if (keys_map['a'] || keys_map['A']) camera.processKeyboard(LEFT, deltaTime);
-        if (keys_map['d'] || keys_map['D']) camera.processKeyboard(RIGHT, deltaTime);
+        if (keys_map['w']) camera.processKeyboard(FRONT, deltaTime);
+        if (keys_map['s']) camera.processKeyboard(BACK, deltaTime);
+        if (keys_map['a']) camera.processKeyboard(LEFT, deltaTime);
+        if (keys_map['d']) camera.processKeyboard(RIGHT, deltaTime);
         if (keys_map[27]) glutLeaveMainLoop(); // ESC
         if (keys_map[GLUT_KEY_UP + IS_SPECIAL_KEY]) camera.processKeyboard(FRONT, deltaTime);
         if (keys_map[GLUT_KEY_DOWN + IS_SPECIAL_KEY]) camera.processKeyboard(BACK, deltaTime);
